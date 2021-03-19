31f0ea2eb2538b1170e7373db237b814919c053a
Eden Zafran <73082175+edenzafran@users.noreply.github.com>
2021-03-18 19:51:11 -0700

    Add files via upload

---
+++ colebrook_equation_solver.m
@@ -0,0 +1,28 @@
+%{
+Solves Colebrook Formula 
+
+used for MAE103 @ UCLA W20
+
+Eden Zafran 
+3/16/21
+%}
+clc; clear all; close all;
+
+%% input values NOTE: make sure units match!
+% roughness of surface (0 for smooth pipe)
+e = 0.0005;
+% diameter of pipe
+D = 8/12;
+% Reynolds number
+Re = 2.19E5;
+
+%% solve for friction factor f
+% first guess b/c ~middle of Moody chart
+f = 0.02;
+
+% 100 iterations ~sufficient for convergence
+for j=1:100
+    f = (-2.0*log10( ( (e/D)/3.7) + (2.51/(Re*sqrt(f)) ) ))^-2; 
+end
+%% output
+fprintf("f = %f\n", f);
